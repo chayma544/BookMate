25b8e4323a0d6486edb3686edea08f8206bc643f
chayma <chaymalabiadh544@gmail.com>
2025-04-06 14:09:14 +0100

    task2 angular

--- front/src/app/models/request.model.ts
+++ front/src/app/models/request.model.ts
@@ -1,2 +1,13 @@
-export class Request {
+export interface Request {
+  requester_id: number;
+  book_id: number;
+  book_title?: string; // Added for display purposes
+  book_author?: string; // Added for display purposes
+  type: 'BORROW' | 'TRADE' | 'GIFT';
+  status: 'PENDING' | 'APPROVED' | 'REJECTED' | 'COMPLETED';
+  date_request: string;
+  date_processed?: string;
+  processed_by?: number;
+  //Tracks who handled the request - Stores the user ID of the admin/moderator who approved, rejected, or completed the request
+  message?: string;
 }
--- front/src/app/models/user.model.ts
+++ front/src/app/models/user.model.ts
@@ -1,2 +1,19 @@
-export class User {
-}
+//we use a class when there are methods inside
+//in our case we use interfaces considering it's more flexible (allow objects to be created dynamically without requiring a constructor)
+export interface User {
+    FirstName: string;
+    LastName: string;
+    Age?: number;
+    //the age is optional
+    favorite_genres?: string[];//add this to the user table
+    joined_date?: string;
+  }
+  
+  export interface UserBook {
+    user_id: number;
+    book_id: number;
+    added_date: string;
+    status: 'READ' | 'READING' | 'TO_READ' | 'FAVORITE';
+    rating?: 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 4.5 | 5;
+    review?: string;
+  }
