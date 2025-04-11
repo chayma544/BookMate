dd6c8338ea9144ed2d2d6dbd7edec3b07b461d37
chayma <chaymalabiadh544@gmail.com>
2025-04-11 11:20:08 +0100

    components

---
+++ front/src/app/components/reading-section/reading-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading-section',
+  imports: [],
+  templateUrl: './reading-section.component.html',
+  styleUrl: './reading-section.component.css'
+})
+export class ReadingSectionComponent {
+
+}
---
+++ front/src/app/components/welcome-banner/welcome-banner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-banner',
+  imports: [],
+  templateUrl: './welcome-banner.component.html',
+  styleUrl: './welcome-banner.component.css'
+})
+export class WelcomeBannerComponent {
+
+}
