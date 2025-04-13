1cd61fb64527e7a38dad1ae926f7d0f351a5035b
chayma <chaymalabiadh544@gmail.com>
2025-04-13 12:50:57 +0100

    c

--- front/src/app/components/reading-section/reading-section.component.ts
+++ front/src/app/components/reading-section/reading-section.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Book } from '../../models/Book.model';
 import { BookItemComponent } from '../book-item/book-item.component'; 
-
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-reading-section',
   templateUrl: './reading-section.component.html',
   standalone: true,
-  imports: [BookItemComponent],
+  imports: [BookItemComponent,NgFor],
   styleUrls: ['./reading-section.component.css']
 })
 export class ReadingSectionComponent {
