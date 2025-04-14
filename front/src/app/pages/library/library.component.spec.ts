b53d1ca89c1a4ea2f83c8524bfe5d1cf1057f8ba
rania <rania.makhlouf4607@istic.ucar.tn>
2025-04-14 22:41:24 +0100

    book component front set

--- front/src/app/pages/library/library.component.spec.ts
+++ front/src/app/pages/library/library.component.spec.ts
@@ -1,19 +1,18 @@
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ReadingSectionComponent } from './reading-section.component';
+import { LibraryComponent } from './library.component';
 
-describe('ReadingSectionComponent', () => {
-  let component: ReadingSectionComponent;
-  let fixture: ComponentFixture<ReadingSectionComponent>;
+describe('LibraryComponent', () => {
+  let component: LibraryComponent;
+  let fixture: ComponentFixture<LibraryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReadingSectionComponent]
+      imports: [LibraryComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ReadingSectionComponent);
+    fixture = TestBed.createComponent(LibraryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
